b852395138bb1c450861669717b98cf55034b692
jsanchez <jsanchez@peopleofthe.cloud>
2024-01-30 10:22:41 +0100

    Upload

---
+++ WebApplication2/Controllers/PruebaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PruebaController : Controller
+    {
+        public IActionResult Load()
+        {
+            PruebaModel model = new PruebaModel();
+            model.id = 1;
+            model.name = "Test";
+
+            return PartialView("_DatosPersonales", model);
+        }
+    }
+}
